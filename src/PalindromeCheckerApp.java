d00bdb71a5dcb8d815a0a76ac1ea0afb73cd40bb
shubh-2006 <shubhagarwal206@gmail.com>
2026-03-05 03:04:26 +0530

    UC7: Deque Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,22 +3,20 @@ import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String input = "civic";
+        String input = "refer";
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new ArrayDeque<>();
 
         for(char c : input.toCharArray())
         {
-            queue.add(c);
-            stack.push(c);
+            deque.add(c);
         }
 
         boolean isPalindrome = true;
 
-        while(!queue.isEmpty())
+        while(deque.size() > 1)
         {
-            if(queue.remove() != stack.pop())
+            if(deque.removeFirst() != deque.removeLast())
             {
                 isPalindrome = false;
                 break;
